4b426cab4a5f10ef5aa3cf715a8e636835df6500
ES-Good <46601455+ES-Good@users.noreply.github.com>
2020-12-07 02:30:29 +0400

    'slider-girl'

--- js/common.js
+++ js/common.js
@@ -6,3 +6,34 @@ new fullpage('#fullpage', {
 
 //methods
 fullpage_api.setAllowScrolling(true);
+
+$('.slider-girl').slick({
+	prevArrow: '<button class="slick-prev slick-arrow prev-girl" aria-label="Previous" type="button" style="">Previous</button>',
+    nextArrow: '<button class="slick-next slick-arrow next-girl" aria-label="Next" type="button" style="">Next</button>',
+	infinite: false,
+	centerMode: false,
+	speed: 1000,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	initialSlide: 1
+});
+
+$('.slider-girl').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+	changeDescriptionGirl();
+  });
+
+const sizeBoobs = document.getElementById('boobs');
+const ageGirl = document.getElementById('age');
+const nameGirl = document.getElementById('name-girl');
+
+
+function changeDescriptionGirl() {
+	setTimeout(function () {
+		sizeBoobs.innerText = document.querySelector('.slick-active').lastChild.getAttribute('data-boobs');
+		ageGirl.innerText = document.querySelector('.slick-active').lastChild.getAttribute('data-age');
+		nameGirl.innerText = document.querySelector('.slick-active').lastChild.getAttribute('data-name');
+		console.log(document.querySelector('.slick-active').lastChild.getAttribute('data-boobs'))
+	},500)
+}
+
+changeDescriptionGirl();
