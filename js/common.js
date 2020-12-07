2eddb79b029c6d6bd79b7abe25ad091957bbd6c5
unknown <evgenshtanko@ya.ru>
2020-12-07 19:50:41 +0400

    't'

--- js/common.js
+++ js/common.js
@@ -1,7 +1,149 @@
+const btnShowPriceList = document.getElementById('btn-show-price-list');
+const closeList = document.getElementById('close-list');
+
+const descriptioPrice = document.getElementById('description-price');
+const descriptio = document.getElementById('description');
+const listProgramm = document.querySelectorAll('.item-programm');
+
+const price = 
+{
+	Burlesque:{
+		description: '<p>...Пенное джакузи, наслаждение ласками девушки,поцелуи и взбитые сливки слизанные с вашего тела... Сыграем?</p>',
+		descriptionPrice:`<p>C 1 девушкой</p>
+						<p>120 минут</p>
+						<p>6 000 руб.</p>`
+	},
+	FoamyEuphoria:{
+		description: '<p>Самое быстрое наслаждение, заряженное на все 100. Начни рабочий день с небольшого отдыха.</p>',
+		descriptionPrice:`<div>
+						<p>C 1 девушкой</p>
+						<p>30 минут</p>
+						<p>1 900 руб.</p>
+						</div>`
+	},
+	TreatiseDesires:{
+		description: '<p>Воплоти страсти в любой позе Камасутры.</p>',
+		descriptionPrice:`<div>
+							<p>C 1 девушкой</p>
+							<p>60 минут</p>
+							<p>5 600 руб.</p>
+						</div>
+						<div>
+							<p>C 1 девушкой</p>
+							<p>90 минут</p>
+							<p>6 900 руб.</p>
+						</div>`
+	},
+	Temptation:{
+		description: '<p>Самое быстрое наслаждение, заряженное на все 100. Начни рабочий день с небольшого отдыха.</p>',
+		descriptionPrice:`<div>
+							<p>C 1 девушкой</p>
+							<p>60 минут</p>
+							<p>3 000 руб.</p>
+						</div>`
+	},
+	Nonrandomness:{
+		description: '<p>В шаге от соблазнительных поцелуев и нежных покусываний до стретты оральных удовольствий.</p>',
+		descriptionPrice:`<div>
+							<p>C 1 девушкой</p>
+							<p>60 минут</p>
+							<p>4 500 руб.</p>
+						</div>
+						<div>
+							<p>C 1 девушкой</p>
+							<p>90 минут</p>
+							<p>6 200 руб.</p>
+						</div>`
+	},
+	Sirens:{
+		description: '<p>Чувствуешь как они манят тебя? Две девушки,с какой дойти до конца, решать только тебе.</p>',
+		descriptionPrice:`<div>
+							<p>C 2 девушками</p>
+							<p>90 минут</p>
+							<p>8 000 руб.</p>
+						</div>
+						<div>
+							<p>C 2 девушками</p>
+							<p>120 минут</p>
+							<p>11 000 руб.</p>
+						</div>`
+	},
+	WeAreCrazy:{
+		description: '<p>Мои подружки-лесбиянки. Попробуешь стать их другом?</p>',
+		descriptionPrice:`<div>
+							<p>C 2 девушками</p>
+							<p>60 минут</p>
+							<p>10 000 руб.</p>
+						</div>
+						<div>
+							<p>C 2 девушками</p>
+							<p>90 минут</p>
+							<p>12 500 руб.</p>
+						</div>
+						<div>
+							<p>C 2 девушками</p>
+							<p>120 минут</p>
+							<p>15 000 руб.</p>
+						</div>`
+	},
+	TheTamingOfTheShrew:{
+		description: '<p>Ты пробовал совместить боль и ласку,унижение и уважение? Госпожа знает подход к Боссу.</p>',
+		descriptionPrice:`<div>
+							<p>C 1 девушкой</p>
+							<p>60 минут</p>
+							<p>7 000 руб.</p>
+						</div>
+						<div>
+							<p>C 1 девушкой</p>
+							<p>90 минут</p>
+							<p>9 000 руб.</p>
+						</div>
+						<div>
+							<p>C 1 девушкой</p>
+							<p>120 минут</p>
+							<p>11 000 руб.</p>
+						</div>`
+	},
+	Infinity:{
+		description: '<p>Каждый знает об этой программе, потому что составляет её сам. Только лучшие техники на твой выбор.</p>',
+		descriptionPrice:`<div>
+							<p>C 1 девушкой</p>
+							<p>60 минут</p>
+							<p>6 500 руб.</p>
+						</div>
+						<div>
+							<p>C 1 девушкой</p>
+							<p>90 минут</p>
+							<p>8 000 руб.</p>
+						</div>
+						<div>
+							<p>C 1 девушкой</p>
+							<p>120 минут</p>
+							<p>9 900 руб.</p>
+						</div>`
+	},
+	Solo:{
+		description: '<p>Стань режиссёром фильма, где девушка ласкает себя игрушками, не забывая о тебе.</p>',
+		descriptionPrice:`<div>
+							<p>C 1 девушкой</p>
+							<p>60 минут</p>
+							<p>6 500 руб.</p>
+						</div>
+						<div>
+							<p>C 1 девушкой</p>
+							<p>90 минут</p>
+							<p>8 000 руб.</p>
+						</div>`
+	},
+};
+
+
 new fullpage('#fullpage', {
 	//options here
 	autoScrolling:true,
-	scrollHorizontally: true
+	scrollHorizontally: true,
+	loopBottom: true,
+	parallax: true
 });
 
 //methods
@@ -32,8 +174,21 @@ function changeDescriptionGirl() {
 		sizeBoobs.innerText = document.querySelector('.slick-active').lastChild.getAttribute('data-boobs');
 		ageGirl.innerText = document.querySelector('.slick-active').lastChild.getAttribute('data-age');
 		nameGirl.innerText = document.querySelector('.slick-active').lastChild.getAttribute('data-name');
-		console.log(document.querySelector('.slick-active').lastChild.getAttribute('data-boobs'))
 	},500)
 }
 
 changeDescriptionGirl();
+
+btnShowPriceList.onclick = function () {
+	closeList.classList.remove('d-none');
+	btnShowPriceList.classList.add('d-none');
+}
+
+function showDescriptionProgramm(item) {
+	let name = item.getAttribute('data-nameProgramm');
+	return name;
+}
+
+for (let i = 0; i < listProgramm.length; i++) {
+	console.log(typeof showDescriptionProgramm(listProgramm[i]));
+}
